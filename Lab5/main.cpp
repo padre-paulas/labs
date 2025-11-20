6d4c9b2693712c549ff8292c2a366b476bf4e82a
padre_paulas <pounowyou@gmail.com>
2025-11-20 23:36:05 +0200

    [Lab5.task1] Improve algorithm

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -32,12 +32,12 @@ int main() {
   int spaces(0);
   int skip(0);
 
-  while ((ch = getchar()) != '\n'&& ch != EOF && i < 256) {
+  while ((ch = getchar()) != '\n' && ch != EOF && i < 256) {
     // if (ch == *" ") {
     //   spaces++;
     // }
-    fullName[i++] = ch;
-    if (ch == *" ") {
+    // fullName[i] = ch;
+    if (ch == ' ') {
       skip++;
     }
     if (skip == 0) {
@@ -47,37 +47,26 @@ int main() {
     } else if (skip == 2) {
       third[i] = ch;
     }
+    std::cout << skip << std::endl;
+    i++;
   }
   fullName[i] = '\0';
+  first[i] = '\0';
+  second[i] = '\0';
+  third[i] = '\0';
 
   if (strlen(first) > strlen(second) && strlen(first) > strlen(third)) {
-    printf("%s", first);
+    printf("%s\n", first);
+    std::cout << "well comeee" << std::endl;
   } else if (strlen(second) > strlen(third)) {
-    printf("%s", second);
+    printf("%s\n", second);
+    std::cout << "well comeee 2222" << std::endl;
   } else {
-    printf("%s", third);
+    printf("%s\n", third);
+    std::cout << "well comeee 3333" << std::endl;
   }
 
-  // printf("%s\n", fullName);
-  // int longestPos = 0;
-  // char *p = strtok(fullName, " ");
-
-  // for (i = 0; i < strlen(fullName); i++) {
-  //   p = strtok(NULL, " ");
-
-  // }
-
   
-
-  // std::tuple<int, char> first;
-  // std::tuple<int, char> second;
-  // std::tuple<int, char> third;
-
-  // for (i = 0; i < strlen(fullName); i++) {
-  //   if (fullName[i] != *(" ")) {
-  //     i++;
-  //   } 
-  // }
 
   return 0;
 }
