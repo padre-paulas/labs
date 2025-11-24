71e8c314d7bbae0e9f5c81a8116da42f0252a41e
padre_paulas <pounowyou@gmail.com>
2025-11-24 19:09:36 +0200

    [lab5.task2] Create task2

---
+++ Lab6/task2.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+
+
+
+int main() {
+
+
+
+  return 0;
+}
+
+void getSentence() {
+
+  char sentence[256];
+
+  fputs("Hi! This program sorts the words by length in the sentence you input.\n", stdout);
+
+  fgets(sentence, 256, stdin);
+
+  
+}
