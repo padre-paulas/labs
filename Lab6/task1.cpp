b7577be48aa9cc710200f428863eba64649229eb
padre_paulas <pounowyou@gmail.com>
2025-11-24 18:55:10 +0200

    [lab5.task1] Finish task 1

--- Lab6/task1.cpp
+++ Lab6/task1.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 
+bool checkString();
 
 int main() {
 
+
+  fputs("\nThis programming checks for double letters.\nEnter a string: ", stdout);
+  bool result = checkString();
+  result ? fputs("Your string has double letters.\n\n", stdout) :
+  fputs("Your string doesn't have double letters.\n\n", stdout);
+
+  return 0;
+}
+
+bool checkString() {
   char *string = new char[256];
  
   fgets(string, 256, stdin);
-  fputs(string, stdout);
+  int len = strlen(string) - 1;
 
+  for (int i = 0; i < len; i++) {
+    if (string[i] == string[i+1]) {
+      delete[] string;
+      return true;
+    }
+  }
   delete[] string;
-
-
-  return 0;
+  return false;
 }
