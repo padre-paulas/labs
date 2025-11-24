40561e04755af3184d2c48eb9764efdc63536496
padre_paulas <pounowyou@gmail.com>
2025-11-24 20:30:23 +0200

    [lab5.task2] Finish task2, assign words to pointers in an array, add a simple sorting algorithm

--- Lab6/task2.cpp
+++ Lab6/task2.cpp
@@ -14,7 +14,7 @@ char* getSentence() {
 
   char *sentence = new char[256];
 
-  fputs("Hi! This program sorts the words by length in the sentence you input.\n", stdout);
+  fputs("\nHi! This program sorts the words by length in the sentence you input.\n", stdout);
   fputs("Enter a sentence: ", stdout);
 
   fgets(sentence, 256, stdin);
@@ -23,14 +23,47 @@ char* getSentence() {
 }
 
 void sortWords(char *sentence) {
-  char *ptr[256][100];
 
-  int i = 0;
+  std::cout << "well well well\n";
 
-  while (true) {
-    if (sentence[i] == '0') break;
-    *ptr[i][0] = sentence[i];
+  char *words[256];
+  int w = 0, ch = 0;
+  int longestWord = 0;
 
+  words[w] = new char[256];
+
+  for (int i = 0; sentence[i] != '\0'; i++) {
+    if (sentence[i] == ' ' || sentence[i] == '\n') {
+      if (ch > 0) {
+        words[w][ch] = '\0';
+        w++;
+        words[w] = new char[256];
+        ch = 0;
+      }
+    } else {
+      words[w][ch++] = sentence[i];
+    }
+  }
+
+  if (ch > 0) {
+    words[w][ch] = '\0';
+  }
+  char *temp;
+
+  for (int i = 0; i < w; i++) {
+    for (int j = 0; j < w - 1; j++) {
+      if (strlen(words[j]) > strlen(words[j + 1])) {
+        temp = words[j];
+        words[j] = words[j + 1];
+        words[j + 1] = temp;
+      }
+    }
+  }
+
+  for (int i = 0; i < w; i++) {
+    fputs(words[i], stdout);
+    fputs("\n", stdout);
+    delete[] words[i];
   }
 
   delete[] sentence;
