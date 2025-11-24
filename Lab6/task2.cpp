67fa4bdf34511ec1fd3d60eed8825eab4db5ad24
padre_paulas <pounowyou@gmail.com>
2025-11-24 19:21:05 +0200

    [lab5.task2] Add functions geSentence and sortWords

--- Lab6/task2.cpp
+++ Lab6/task2.cpp
@@ -1,21 +1,37 @@
 #include <iostream> 
 
-
+char* getSentence();
+void sortWords(char *sentence);
 
 int main() {
 
-
+  sortWords(getSentence());
 
   return 0;
 }
 
-void getSentence() {
+char* getSentence() {
 
-  char sentence[256];
+  char *sentence = new char[256];
 
   fputs("Hi! This program sorts the words by length in the sentence you input.\n", stdout);
+  fputs("Enter a sentence: ", stdout);
 
   fgets(sentence, 256, stdin);
 
-  
+  return sentence;
+}
+
+void sortWords(char *sentence) {
+  char *ptr[256][100];
+
+  int i = 0;
+
+  while (true) {
+    if (sentence[i] == '0') break;
+    *ptr[i][0] = sentence[i];
+
+  }
+
+  delete[] sentence;
 }
