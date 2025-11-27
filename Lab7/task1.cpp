8f371467312cf7a670957dde38db1258cc83b875
padre_paulas <pounowyou@gmail.com>
2025-11-27 23:05:09 +0200

    [lab5.task1] Update task 1

--- Lab7/task1.cpp
+++ Lab7/task1.cpp
@@ -1,10 +1,24 @@
 #include <iostream> 
 
-
+double sum(double A[][5]);
 
 int main() {
 
+  double A[][5] = {{123.4}};
 
+  double s = sum(A);
+  std::cout << s << std::endl;
 
   return 0;
+}
+
+double sum(double A[][5]) {
+  double s = 0;
+  int i, j;
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 5; j++) {
+      s += A[i][j];
+    }
+  }
+  return s;
 }
