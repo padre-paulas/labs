8f4509d452ef27bd7ee0172cbed8a2c47aef8f44
padre_paulas <pounowyou@gmail.com>
2025-11-29 15:43:20 +0200

    [lab7.task1] Create functions to calculate integral

--- Lab7/task1.cpp
+++ Lab7/task1.cpp
@@ -1,24 +1,55 @@
 #include <iostream> 
 
-double sum(double A[][5]);
+// double getX();
+std::pair<double, double> getInterval();
+double calculateIntegral(std::pair<double, double> interval, int step);
+void printResult(double result);
 
 int main() {
 
-  double A[][5] = {{123.4}};
-
-  double s = sum(A);
-  std::cout << s << std::endl;
+  double result = calculateIntegral(getInterval(), 0);
+  printResult(result);
 
   return 0;
 }
 
-double sum(double A[][5]) {
-  double s = 0;
-  int i, j;
-  for (i = 0; i < 4; i++) {
-    for (j = 0; j < 5; j++) {
-      s += A[i][j];
-    }
-  }
-  return s;
+// double getX() {
+
+//   double x;
+
+//   std::fputs("-----------------------------------------", stdout);
+//   std::fputs("This program calculates integral of \n7x^3 - x^2 + 3x + 2 on interval [0, pi]\n", stdout);
+
+//   // std::fputs("Enter x: ", stdout);
+//   // std::cin >> x;
+
+//   return x;
+// }
+
+std::pair<double, double> getInterval() {
+  double lowX = 2, highX = 3;
+  int step = 0;
+  
+  return std::pair<double, double> (lowX, highX);
+}
+
+double calculateIntegral(std::pair<double, double> interval, int step) {
+
+  double lowX = interval.first;
+  double highX = interval.second;
+
+  double x = lowX;
+  int accuracy = 1000;
+  double deltaX = x / accuracy;
+
+  std::cout << step << std::endl;
+
+  if (step == accuracy - 1) {
+    return deltaX * (7 * pow(x, 3) - pow(x, 2) + 3 * x + 2);
+  } else return calculateIntegral(interval, ++step) + deltaX * (7 * pow(x, 3) - pow(x, 2) + 3 * x + 2);
+
+}
+
+void printResult(double result) {
+  std::cout << "Result: " << result << std::endl;
 }
