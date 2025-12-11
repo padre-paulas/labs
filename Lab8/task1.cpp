7fb3693d248705c4c91e637a5a8e574bd162ba7d
padre_paulas <pounowyou@gmail.com>
2025-12-11 21:18:36 +0200

    [Lab8.task1] Add readFile function

--- Lab8/task1.cpp
+++ Lab8/task1.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
+
+void readFile();
 
 int main() {
 
 
   return 0;
+}
+
+void readFile() {
+  std::fstream fs;
+  std::string fileName = "student.txt";
+
+  fs.open(fileName, std::ios::in);
+
 }
 
