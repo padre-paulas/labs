01653a94c78922653ce8670b5036c2907c0deaec
padre_paulas <pounowyou@gmail.com>
2025-12-18 23:54:20 +0200

    [Lab8.task1] Finish the functions

--- Lab8/task1.cpp
+++ Lab8/task1.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 void readFile();
-void calculateAverage();
+
+struct Student {
+  std::string lastName;
+  int grade1, grade2, grade3, grade4;
+  double averageGrade;
+
+  void calculateAverage() {
+    averageGrade = (grade1 + grade2 + grade3 + grade4) / 4.0;
+  }
+};
 
 int main() {
 
-
+  readFile();
+  
   return 0;
 }
 
+
+
 void readFile() {
-  std::fstream fs;
-  std::string fileName = "student.txt";
+  std::ifstream file("student.txt");
+  std::vector<Student> students;
+  double overallAverage = 0;
 
-  fs.open(fileName, std::ios::in);
+  if (!file) {
+    std::cout << "File not found\n";
+    return;
+  }
 
-  fs.close();
+  Student s;
 
+  while (file >> s.lastName >> s.grade1 >> s.grade2 >> s.grade3 >> s.grade4) {
+    s.calculateAverage();
+    students.push_back(s);
+  }
+
+  int i = 0;
+  for (const Student &st : students) {
+    overallAverage += st.averageGrade;
+    i++;
+  }
+
+  overallAverage /= i;
+
+  std::cout << "\nOverall average = " << overallAverage << "\n\n";
+
+  for (const Student &st : students) {
+    if (st.averageGrade > overallAverage) {
+      std::cout << st.lastName << " " << st.averageGrade << std::endl;
+    }
+  }
+  std::cout << std::endl;
 }
-
-void calculateAverage() {
-  struct Student {
-    std::string lastName;
-    int grade1, grade2, grade3, grade4;
-  };
-}
-
