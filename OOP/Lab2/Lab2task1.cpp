0a17e1fb0a76e6aea95b2d8957a28af041c644f3
padre_paulas <pounowyou@gmail.com>
2026-02-11 15:34:47 +0200

    create constructors and methods for BankAcount class

--- OOP/Lab2/Lab2task1.cpp
+++ OOP/Lab2/Lab2task1.cpp
@@ -5,10 +5,47 @@
 
 int main() {
 
-  BankAccount first;
-  first.print();
-  // BankAccount second("Pavlo");
-
+  // BankAccount first;
+  // first.print();
+  BankAccount second("Pavlo");
+  // second.print();
+  // second.setName("Someone");
+  second.depositWithdraw(3, true);
+  second.print();
+  second.depositWithdraw(3, false);
+  second.print();
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();  
+  second.setName("Emma");
+  second.print();  
+  second.setName("Emma");
+  second.print();  
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();
+  second.depositWithdraw(150, true);
+  second.print();
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();  
+  second.setName("Emma");
+  second.print();  
+  second.setName("Emma");
+  second.print();  
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();
+  second.setName("Emma");
+  second.print();
   return 0;
 }
 
@@ -29,33 +66,127 @@ void BankAccount::setDateCreated() {
 }
 
 void BankAccount::print() {
-  std::cout << "Owner: " << owner << std::endl
+  std::cout << "\nOwner: " << owner << std::endl
   << "Account created: " << dateCreated.day 
   << "/" << dateCreated.month << "/" << dateCreated.year 
   << std::endl << "Balance: " << balance << std::endl 
-  << "Recent actions: " << recentActions << std::endl;
+  << "Recent actions: ";
+
+  int i = 0, j = 0;
+  while (recentActions[i][0] != '\0' && i < 10) {
+    std::cout << std::endl << i + 1 << ". ";
+    j = 0;
+    while (recentActions[i][j] != '\0' && j < 50) {
+      std::cout << recentActions[i][j];
+      j++;
+    }
+    i++;
+    std::cout << " ";
+  } 
+  std::cout << "\n\n";
 };
+
+void BankAccount::setName(const char newName[20]) {
+  int i = 0;
+  while (newName[i] != '\0' && i < 19) {
+    owner[i] = newName[i];
+    i++;
+  }
+  owner[i] = '\0';
+  char buffer[50];
+  std::snprintf(buffer, 50, "Changed name to %s", newName);
+  updateRecentActions(buffer);
+};
+
+void BankAccount::depositWithdraw(int amount, bool deposit) {
+  if (amount <= 0) {
+    std::cout << "Invalid input!" << std::endl;
+    return;
+  }
+  if (deposit) {
+    balance += amount;
+    char buffer[50];
+    std::snprintf(buffer, 50, "Deposited %u$", amount);
+    updateRecentActions(buffer);
+    return;
+  }
+  if (amount > balance) {
+    std::cout << "Not enough money!" << std::endl;
+    return;
+  }
+  char buffer[50];
+  std::snprintf(buffer, 50, "Withdrew %u$", amount);
+  updateRecentActions(buffer);
+  balance -= amount;
+  return;
+}
+
+void BankAccount::updateRecentActions(const char sentence[50]) {
+  int i = 0, j = 0;
+  while (i < 10) {
+    std::cout << "At the top i = " << i << " \n";
+    j = 0;
+    if (recentActions[i][0] == '\0') {
+      while (sentence[j] != '\0' && j < 49) {
+        recentActions[i][j] = sentence[j];
+        // std::cout << "recent actions[i][j] = " << recentActions[i][j] 
+        // << "\tsentence[j] = " << sentence[j] << std::endl;
+
+        j++;
+      }
+      recentActions[i][j] = '\0';
+
+      return;
+    }
+    // std::cout << "Here's recentActions[9][j] = " << recentActions[9][j];
+    // std::cout << "\njust above i++ i = " << i << " \n";
+    i++;
+    std::cout << "\ni = " << i << " \n";
+  }
+  std::cout << "hello are we here? ";
+  j = 0;
+  if (recentActions[9][0] != '\0') {
+    for (i = 1; i < 10; i++) {
+      while (j < 49 && recentActions[i][j] != '\0') {
+        recentActions[i - 1][j] = recentActions[i][j];
+        j++;
+      }
+      recentActions[i][j] = '\0';
+    }
+
+    j = 0;
+    while (sentence[j] != '\0' && j < 49) {
+      recentActions[9][j] = sentence[j];
+      j++;
+    }
+  }
+}
 
 BankAccount::BankAccount() 
 : owner("Undefined"),
-balance(0), 
-recentActions({"Account created"}) {
+balance(0) {
+  for(int i = 0; i < 10; i++) {
+    recentActions[i][0] = '\0'; 
+  }
   setDateCreated();
 };
 
-BankAccount::BankAccount(char accOwner[20])
-: balance(0),
-recentActions({"Account created"}) {
+BankAccount::BankAccount(const char accOwner[20])
+: balance(0) {
   int i = 0;
-  while (accOwner[i] != '\0' && i < 20) {
+  while (accOwner[i] != '\0' && i < 19) {
     owner[i] = accOwner[i];
     i++;
   }
-  owner[i] = '/0';
+  owner[i] = '\0';
+
+  for(int i = 0; i < 10; i++) {
+    recentActions[i][0] = '\0'; 
+  }
 
   setDateCreated();
-
 };
+
 
 
 
