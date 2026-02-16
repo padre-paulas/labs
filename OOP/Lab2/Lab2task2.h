8490742455fa3a4e579d371dbed32173418ff5a5
padre_paulas <pounowyou@gmail.com>
2026-02-16 22:29:23 +0200

    rename header file

--- OOP/Lab2/Lab2task2.h
+++ OOP/Lab2/Lab2task2.h
@@ -2,19 +2,19 @@
 #define FIRM_H
 #include "Lab2task1.h"
 
-
 class BankAccounts {
   private:
     static const unsigned maxAccNumber = 100;
     BankAccount accounts[maxAccNumber];
     unsigned actualAccNumber = 0;
   public: 
-    void addAcc();
+    void addAcc(const char accOwner[20]);
     void editAcc();
-    void deleteAcc();
+    void deleteAcc(int ID);
     void printPersonsAccs();
     void printAllAccs();
     void sortAccs();
+    BankAccounts();
 };
 
 
