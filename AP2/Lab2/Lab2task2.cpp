6a039136c4fd4ce3f7f0ec480aa77bfef7deee94
padre_paulas <pounowyou@gmail.com>
2026-03-09 14:56:14 +0200

    add functions to handle stack, correct previous functions

--- AP2/Lab2/Lab2task2.cpp
+++ AP2/Lab2/Lab2task2.cpp
@@ -7,21 +7,38 @@ struct Node {
     double price;
     Node *next;
   public:
-    Node(int value, Node *next);
+    Node(std::string phoneName, double price, Node *next);
     void print();
     friend class Stack;
 };
 
 class Stack {
   private: 
+  public: // to be returned to private!!
     Node *top;
   public: 
     Stack() { top = nullptr; };
-    ~Stack();
-    void push(int value);
-    Node* pop();
-    Stack* sort();
+    // ~Stack();
+    void push(std::string phoneName, double price);
+    void pop();
+    void deleteFirst();
+    void printStack();
+    void clearStack();
+    // Stack* sort();
 };
+
+int main() {
+  Stack stackOfNodes;
+  stackOfNodes.push("Iphone 13", 500);
+  stackOfNodes.push("Iphone 14", 500);
+  stackOfNodes.push("Iphone 15", 600);
+  stackOfNodes.push("Iphone 17", 1000);
+  stackOfNodes.top->print();
+  stackOfNodes.clearStack();
+  stackOfNodes.printStack();
+
+  return 0;
+}
 
 // int main() {
 //   Stack stackOfNodes;
@@ -38,29 +55,44 @@ class Stack {
 //   return 0;
 // }
 
-// Node::Node(int value, Node *next) : value(value), next(next) {};
+Node::Node(std::string phoneName, double price, Node *next)
+: phoneName(phoneName), price(price), next(next) {};
 
-// void Node::print() {
-//   std::cout << "Value: " << this->value << std::endl;
-// }
+void Node::print() {
+  std::cout << "Phone name: " << phoneName 
+  << ", price: " << price << "$" << std::endl;
+}
 
-// void Stack::push(int value) {
-//   Node *temp = new Node(value, top);
-//   top = temp;
-//   // delete temp;
-//   // std::cout << "We just pushed " << top->value;
-// }
+void Stack::push(std::string phoneName, double price) {
+  Node *temp = new Node(phoneName, price, top);
+  top = temp;
+}
 
-// Node* Stack::pop() {
-//   if (!top) {
-//     std::cout << "Stack is empty!\n";
-//     return nullptr;
-//   }
-//   Node *temp = top;
-//   top = top->next;
-//   // std::cout << "THE VALUE: " << temp->value;
-//   return temp;
-// }
+void Stack::pop() {
+  if (!top) {
+    std::cout << "Stack is empty!\n";
+    return;
+  }
+  Node *temp = top;
+  top = top->next;
+  delete temp;
+}
+
+void Stack::printStack() {
+  Node *current = top;
+  while (current) {
+    current->print();
+    current = current->next;
+  }
+}
+
+void Stack::clearStack() {
+  while (top) pop();
+}
+
+void Stack::deleteFirst() {
+
+}
 
 // // Stack* Stack::sort() {
 // //   Stack copy = *this;
