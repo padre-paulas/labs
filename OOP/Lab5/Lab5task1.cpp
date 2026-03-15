4a3e6699371e52e0a0cec72a5f7f53b657c7d6eb
padre_paulas <pounowyou@gmail.com>
2026-03-15 14:52:32 +0200

    write the demo function

--- OOP/Lab5/Lab5task1.cpp
+++ OOP/Lab5/Lab5task1.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
 
+void demo();
+
 class Airplane_B1 {
   protected:
     double weight;
   public:
     Airplane_B1(double weight) : weight(weight) {};
     void show() {
-      std::cout << "Airplane B1:\nWeight: " << weight << std::endl;
+      std::cout << "Airplane B1:\nWeight: " << weight << " kg\n\n";
     };
 };
 
@@ -19,11 +21,11 @@ class Pilot_B2 {
     Pilot_B2(std::string name, int age) : name(name), age(age) {};
     void show() {
       std::cout << "Pilot B2:\nName: " << name 
-      << "\nAge: " << age << std::endl;
+      << "\nAge: " << age << "\n\n";
     };
 };
 
-class PassengerAirliner_D1: public Airplane_B1 {
+class PassengerAirliner_D1: virtual public Airplane_B1 {
   protected:
     int passengerCapacity;
   public:
@@ -32,11 +34,11 @@ class PassengerAirliner_D1: public Airplane_B1 {
     passengerCapacity(passengerCapacity) {};
     void show() {
       std::cout << "PassengerAirliner D1:\nPassenger capacity: " 
-      << passengerCapacity << "\nWeight: " << weight << std::endl;
+      << passengerCapacity << "\nWeight: " << weight << " kg\n\n";
     };
 };
 
-class Airbus_D2: private Airplane_B1 {
+class Airbus_D2: virtual private Airplane_B1 {
   protected:
     double cost;
   public:
@@ -44,8 +46,8 @@ class Airbus_D2: private Airplane_B1 {
     Airplane_B1(weight),
     cost(cost) {};
     void show() {
-      std::cout << "Airbus D2:\nCost: " << cost 
-      << "\nWeight: " << weight << std::endl;
+      std::cout << "Airbus D2:\nCost: $" << cost 
+      << " million\nWeight: " << weight << " kg\n\n";
     };
 };
 
@@ -63,23 +65,37 @@ public Pilot_B2 {
       std::string pilotName, 
       int pilotAge, 
       std::string modelName
-    ) : Airbus_D2(weight, cost),
+    ) : Airplane_B1(weight),
+    Airbus_D2(weight, cost),
     PassengerAirliner_D1(weight, passengerCapacity),
     Pilot_B2(pilotName, pilotAge),
     modelName(modelName) {};
 
     void show() {
       std::cout << "AirbusModel D4:\nModel name: " << modelName 
-      << "\nWeight: " << PassengerAirliner_D1::weight 
-      << "\nPassengerCapacity: " << passengerCapacity 
-      << "\nCost: " << cost
-      << "\nPilot name: " << name 
-      << "\nPilot age: " << age << std::endl;
+      << "\nWeight: " << weight 
+      << " kg\nPassengerCapacity: " << passengerCapacity 
+      << "\nCost: $" << cost
+      << " million\nPilot name: " << name 
+      << "\nPilot age: " << age << "\n\n";
     };
 };  
 
 int main() {
-
-
+  demo();
   return 0;
+}
+
+void demo() {
+  std::cout << std::endl;
+  Airplane_B1 airplane(29300);
+  airplane.show();
+  Pilot_B2 pilot("Martin", 37);
+  pilot.show();
+  PassengerAirliner_D1 passengerAirliner(12000, 298);
+  passengerAirliner.show();
+  Airbus_D2 airbus(190000, 321);
+  airbus.show();
+  AirbusModel_D4 airbusModel(276800, 525, 445, "Andrew", 42, "A380");
+  airbusModel.show();
 }
