a58c38c180fac17351bd0bf8ef18f1500f3bf0d9
padre_paulas <pounowyou@gmail.com>
2026-03-23 16:15:45 +0200

    add template function to handle user-given types

--- AP2/Lab3/Lab3.cpp
+++ AP2/Lab3/Lab3.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
+
+template <typename T>
+class Queue;
 
 template <typename T>
 struct Node {
-  // private:
+  private:
     T value;
     Node *next;
     Node(T value, Node<T> *next = nullptr) 
     : value(value), next(next) {};
-    // template <typename T>
-    // friend class Queue;
+    friend class Queue<T>;
 };
 
 template <typename T>
 class Queue {
   private: 
+  
     Node<T> *front;
     Node<T> *rear;
+    
   public: 
     Queue() : front(nullptr), rear(nullptr) {};
+    ~Queue();
     void enqueue(T value);
     void dequeue();
     T& getFront() const;
@@ -26,18 +32,31 @@ class Queue {
     void display() const;
 };
 
+void demo();
+template <typename T>
+void runMenu(std::string &choice);
 
 int main() {
 
-  // std::cout << "C++ is running\n";
-  Queue<const double> q;
-  q.enqueue(4);
-  q.enqueue(4.5);
-  q.display();
-  q.dequeue();
-  q.display();
+  // Queue<const double> q;
+  // q.enqueue(4);
+  // q.enqueue(4.5);
+  // q.enqueue(89);
+  // q.display();
+  // q.dequeue();
+  // std::cout << q.getFront();
+  // q.display();
+
+  demo();
 
   return 0;
+}
+
+template <typename T>
+Queue<T>::~Queue() {
+  while (!isEmpty()) {
+    this->dequeue();
+  }
 }
 
 template <typename T>
@@ -60,7 +79,6 @@ void Queue<T>::enqueue(T value) {
 template <typename T>
 void Queue<T>::dequeue() {
   if (isEmpty()) {
-    std::cout << "The queue is empty!\n";
     return;
   }
   Node<T> *temp = front;
@@ -72,10 +90,9 @@ void Queue<T>::dequeue() {
 template <typename T>
 T& Queue<T>::getFront() const {
   if (isEmpty()) {
-    std::cout << "The queue is empty!\n";
-    return;
+    throw std::out_of_range("Queue is empty");
   }
-  return front;
+  return front->value;
 }
 
 template <typename T>
@@ -85,8 +102,64 @@ void Queue<T>::display() const {
     return;
   }
   Node<T> *temp = front;
-  while (temp->next != nullptr) {
+  
+  std::cout << "\n⬇ FRONT\n";
+  while (temp != nullptr) {
     std::cout << temp->value << std::endl;
     temp = temp->next;
   }
+  std::cout << "⬆ REAR\n\n";
+}
+
+void demo() {
+  std::string choice;
+  std::cout << "\n=======Queue=======\n";
+  std::cout << "Choose queue data type (str, int, double, char): ";
+  std::cin >> choice;
+
+  if (choice == "str") {
+    runMenu<std::string>(choice);
+  } else if (choice == "int") {
+    runMenu<int>(choice);
+  } else if (choice == "double") {
+    runMenu<double>(choice);
+  } else if (choice == "char") {
+    runMenu<char>(choice);
+  } else {
+    std::cout << "Invalid input!\n";
+    std::cout << "\n===================\n";
+    exit(-1);
+  }
+  std::cout << "\n===================\n";
+}
+
+template <typename T>
+void runMenu(std::string &choice) {
+  Queue<T> queue;
+  T value;
+  do {
+    std::cout << "Choose an operation (enq, deq, getF, isEm): ";
+    std::cin >> choice;
+    
+    if (choice == "enq") {
+      std::cout << "Enter your value: ";
+      std::cin >> value;
+      queue.enqueue(value);
+      queue.display();
+    } else if (choice == "deq") {
+      queue.dequeue();
+      queue.display();
+    } else if (choice == "getF") {
+      try {
+        std::cout << "Front element: " << queue.getFront() << std::endl;
+      } catch (std::out_of_range &e) {
+        std::cout << e.what() << std::endl;
+      }
+    } else if (choice == "isEm") {
+      std::cout << (queue.isEmpty() ? "true" : "false") << std::endl;
+    }
+
+    std::cout << "Continue/exit (c/e): ";
+    std::cin >> choice;
+  } while (choice == "c");
 }
